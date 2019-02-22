915f3d6cfbbc70b6b4c9c1726dd039a951c559fe
Marlon Henry Schweigert <marlon.henry@magrathealabs.com>
2019-02-21 22:31:17 -0300

    Update order.rb

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,5 +2,5 @@ class Order < ApplicationRecord
     has_many :order_products
     has_many :products, through: :order_products
     accepts_nested_attributes_for :products, reject_if: :all_blank, allow_destroy: true
-    enum status: [:active, :archieve]
+    enum status: { active: 0, archieve: 1 }
 end
