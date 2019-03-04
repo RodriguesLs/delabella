b59291c5b78a6c894424ce611401f6054f9de32f
Rodrigues <fr.costa95@gmail.com>
2019-03-04 10:26:04 -0300

    Adjusting devise controller

--- app/controllers/clientes_controller.rb
+++ app/controllers/clientes_controller.rb
@@ -1,4 +1,4 @@
-class ClientesController < ApplicationController
+class Clientes::RegistrationsController < Devise::RegistrationsController
     before_action :configure_permitted_parameters
 
     protected
