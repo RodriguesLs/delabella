d859143438ece1749da95785ef0c5e1251605a75
Rodrigues <fr.costa95@gmail.com>
2019-03-04 11:27:24 -0300

    Migrate AddCpfFields

--- app/controllers/clientes_controller.rb
+++
@@ -1,9 +0,0 @@
-class Clientes::RegistrationsController < Devise::RegistrationsController
-    before_action :configure_permitted_parameters
-
-    protected
-  
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:cliente, :password, :password_confirmation, :cpf])
-    end
-end
---
+++ db/migrate/20190304142651_add_fields_to_clientes.rb
@@ -0,0 +1,5 @@
+class AddFieldsToClientes < ActiveRecord::Migration[5.0]
+  def change
+    add_column :clientes, :cpf, :string
+  end
+end
